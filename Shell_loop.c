48e164c78bfda9d32e7c0221f92fc33c7ce6dac6
ilefde <49443145+ilefde@users.noreply.github.com>
2023-04-20 00:50:12 +0800

    Update Shell_loop.c

--- Shell_loop.c
+++ Shell_loop.c
@@ -6,13 +6,20 @@
  *
  * Return: prompt user to enter command.
  */
-int *void simple_shell(int ac __attribute__((unused)), char **av __attribute__((unused)))
+int main(int ac __attribute__((unused)), char **av __attribute__((unused)))
 {
+  int_mode=1;
   while (int_mode)
   {
   int_mode = isatty(STDIN_FILENO);
      if (int_mode == 1)
      {
+	     prompt(); /**function that prints prompt */
+	     user_input = get.line(); /**function takes in user command */
+	     cmd = parser(user_input);
+		     
+		     
+	     
        write(STDOUT_FILENO, “~$ “, 13);
        read(STDIN)
      }
