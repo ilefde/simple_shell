28e660b16ec2c3e00e2bb9386f124992f4c352bd
ilefde <49443145+ilefde@users.noreply.github.com>
2023-04-20 00:44:02 +0800

    Update Shell_loop.c

--- Shell_loop.c
+++ Shell_loop.c
@@ -17,3 +17,36 @@ int *void simple_shell(int ac __attribute__((unused)), char **av __attribute__((
        read(STDIN)
      }
   }
+/**int 
+     main (int argc, char **argv)
+     {
+	while (1){
+		int childPid;
+		char * cmdLine;
+
+	        printPrompt();
+
+	        cmdLine= readCommandLine(); //or GNU readline("");
+		
+		cmd = parseCommand(cmdLine);
+
+		record command in history list (GNU readline history ?)
+ 
+		if ( isBuiltInCommand(cmd)){
+		    executeBuiltInCommand(cmd);
+		} else {		
+		     childPid = fork();
+		     if (childPid == 0){
+			executeCommand(cmd); //calls execvp  
+			
+		     } else {
+			if (isBackgroundJob(cmd)){
+			        record in list of background jobs
+			} else {
+				waitpid (childPid);
+
+			}		
+		    }
+	        }
+     }
+*/
