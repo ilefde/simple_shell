8b2460020bff9f0ba617180506cd0acd43b5b9b0
ilefde <49443145+ilefde@users.noreply.github.com>
2023-04-29 09:36:05 +0800

    Update loop_prompt.c

--- loop_prompt.c
+++ loop_prompt.c
@@ -1,11 +1,29 @@
-#include 'main.h'
+#include "shell.h"
 
-/** prompt : function that prompts the user to enter commands
-*
-* returns : void 
-*/
-
-void *prompt()
+/**
+ * prompt - Display Shell Prompt
+ */
+void prompt(void)
 {
-  write(1,'$',1);
+	PRINTER("$ ");
+}
+/**
+ * print_error - Display Error Based on Command and How Many Time Shell Looped
+ * @input:User Input
+ * @counter:Simple Shell Count Loop
+ * @argv:Program Name
+ * Return: Void
+ */
+void print_error(char *input, int counter, char **argv)
+{
+	char *er;
+
+	PRINTER(argv[0]);
+	PRINTER(": ");
+	er = _itoa(counter);
+	PRINTER(er);
+	free(er);
+	PRINTER(": ");
+	PRINTER(input);
+	PRINTER(": not found\n");
 }
